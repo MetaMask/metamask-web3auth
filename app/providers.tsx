438c5c07e02fa4f6962818907963e232045db6b8
Mohammad Shahbaz Alam <shahbaz17@users.noreply.github.com>
2025-12-05 18:20:25 +0400

    Bump version
    version of Next.js and web3auth

--- app/providers.tsx
+++ app/providers.tsx
@@ -5,11 +5,7 @@ import { type ReactNode, useState } from "react";
 
 import { Web3AuthProvider } from "@web3auth/modal/react";
 import { WagmiProvider } from "@web3auth/modal/react/wagmi";
-import {
-  WALLET_CONNECTORS,
-  AUTH_CONNECTION,
-  WEB3AUTH_NETWORK,
-} from "@web3auth/modal";
+import { WEB3AUTH_NETWORK } from "@web3auth/modal";
 
 type Props = {
   children: ReactNode;
@@ -23,7 +19,7 @@ export function Providers({ children }: Props) {
       config={{
         web3AuthOptions: {
           clientId: process.env.NEXT_PUBLIC_WEB3AUTH_CLIENT_ID!, // Replace with your Client ID
-          web3AuthNetwork: WEB3AUTH_NETWORK.SAPPHIRE_MAINNET,
+          web3AuthNetwork: WEB3AUTH_NETWORK.SAPPHIRE_DEVNET,
         },
       }}
     >
